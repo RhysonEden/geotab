4c0e08df59ee2b64d848f77cfb0011ac563e1541
James Gale <james@Jamess-Air.attlocal.net>
2021-01-24 18:00:23 -0500

    next

--- db/index.js
+++ db/index.js
@@ -4,9 +4,9 @@ const DB_NAME = "geotab";
 const GeotabApi = require("mg-api-js");
 const string = "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
 
-const DB_URL =
-  `process.env.DATABASE_URL${string}` ||
-  `postgressql://postgres:james@localhost:5432/${DB_NAME}`;
+const DB_URL = `process.env.DATABASE_URL${string}`;
+// ||
+// `postgressql://postgres:james@localhost:5432/${DB_NAME}`;
 
 const client = new Client(DB_URL);
 
